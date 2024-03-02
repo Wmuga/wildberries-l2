73e06039b38a33bcb8626b8b7d118949a170bb07
Wmuga <dsasadf13@gmail.com>
2024-03-02 15:49:16 +0300

    last two patterns

--- pattern/06_factory_method.go
+++ pattern/06_factory_method.go
@@ -1,7 +1,96 @@
 package pattern
 
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
 /*
 	Реализовать паттерн «фабричный метод».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+	Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Factory_method_pattern
 */
+
+/*
+	Применимость:
+		Заранее неизвестно объекты каких типов надо создать
+		Система должна быть независима от процесса создания новых объектов
+		Нуждно делегировать создание новых объектов наследникам
+	Плюсы:
+		Мзбавляет класс от привязки к конкретным классам
+		Выделяет производство в отдельные классы, упрощая поддержку кода
+		Упрощает добавление новых продуктов
+		Принцип открытости / закрытости
+	Минусы:
+		Большие параллельные иерархии классов
+*/
+
+/*
+	Предположим задача:
+		Приложение может работать как и в Web, так на Desktop. Необходимо чтобы в зависимости от платформы создавалась своя кнопка
+	Класс App будет выбирать в зависимости от конфигурации нужный Creator, который создает конкретный Product(Button)
+*/
+
+var (
+	ErrUnknownPlatform = errors.New("unknown platform")
+)
+
+type IButton interface {
+	Click()
+}
+
+type WebButton struct{}
+
+func (*WebButton) Click() {
+	fmt.Println("Web button")
+}
+
+type DesktopButton struct{}
+
+func (*DesktopButton) Click() {
+	fmt.Println("Desktop button")
+}
+
+type IDialog interface {
+	CreateButton() IButton
+}
+
+type WebDialog struct{}
+
+func (w *WebDialog) CreateButton() IButton {
+	return &WebButton{}
+}
+
+type DesktopDialog struct{}
+
+func (w *DesktopDialog) CreateButton() IButton {
+	return &DesktopButton{}
+}
+
+type App struct {
+	dialog IDialog
+}
+
+func (a *App) CreateButton() IButton {
+	return a.dialog.CreateButton()
+}
+
+func NewApp(platform string) (*App, error) {
+	switch platform {
+	case "desktop":
+		return &App{&DesktopDialog{}}, nil
+	case "web":
+		return &App{&WebDialog{}}, nil
+	}
+	return nil, ErrUnknownPlatform
+}
+
+func ExampleFactoryMethodPattern() {
+	app, err := NewApp("desktop")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	button := app.CreateButton()
+	button.Click()
+}
--- pattern/07_strategy.go
+++ pattern/07_strategy.go
@@ -1,7 +1,65 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «стратегия».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
+	Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Strategy_pattern
 */
+
+/*
+	Применимость:
+		Нужно использовать разные вариации одного алгоритма внутри одного объекта
+		Много похожих классов, отличающихся поведением
+		Нужно изолировать код, данные и зависимости от других объектов
+		Различные варианты алгоритма реализованы в виде условного оператора
+	Плюсы:
+		Hot swap алгоритмов на лету
+		Изолирует код и данные алгоритмов от других объектов
+		Уход от наследования к делегированию
+		Принцип открытости / закрытости
+	Минусы:
+		Усложнение дополнительными классами
+		Необходимо знать разницу между стратениями, чтобы выбрать
+*/
+
+/*
+	Предположим задача:
+		Есть архиватор, пользователю необходима возможность выбрать алгоритм шифровки zip, rar, 7z
+	Context - Archiver, Strategy - CompressionAlgorithm, ConcreteStrategy - ZipCompression, RarCompression, SevenZCompression
+*/
+
+type CompressionAlgorithm interface {
+	Compress([]byte) []byte
+}
+
+type ZipCompression struct{}
+
+func (*ZipCompression) Compress(in []byte) []byte {
+	return append(in, []byte(" zipped")...)
+}
+
+type RarCompression struct{}
+
+func (*RarCompression) Compress(in []byte) []byte {
+	return append(in, []byte(" rarred")...)
+}
+
+type SevenZCompression struct{}
+
+func (*SevenZCompression) Compress(in []byte) []byte {
+	return append(in, []byte(" 7zipped")...)
+}
+
+type Archiver struct {
+	Algorithm CompressionAlgorithm
+}
+
+func ExampleStrategyPattern() {
+	arch := Archiver{}
+	arch.Algorithm = &RarCompression{} // и его можно hot swap'пать
+	data := []byte("data")
+	data = arch.Algorithm.Compress(data)
+	fmt.Println(string(data))
+}
