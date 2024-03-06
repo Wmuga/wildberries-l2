5f6f4111240e1807c0011857470c6631a03ace29
Wmuga <dsasadf13@gmail.com>
2024-03-06 15:42:33 +0300

    task 6

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +23,78 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Flags struct {
+	FieldNums []int
+	Delimiter string
+	Separated bool
+}
 
+func GetFlags() Flags {
+	var flags Flags
+	var fieldsStr string
+
+	flag.StringVar(&fieldsStr, "f", "", "выбрать поля (колонки). Разделение по запятой")
+	flag.StringVar(&flags.Delimiter, "d", "\t", "использовать другой разделитель")
+	flag.BoolVar(&flags.Separated, "s", false, "только строки с разделителем")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "cut [OPTION...]")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	flag.Parse()
+
+	splitData := strings.Split(fieldsStr, flags.Delimiter)
+	flags.FieldNums = make([]int, 0, len(splitData))
+	for i := range splitData {
+		val, err := strconv.Atoi(splitData[i])
+		if err != nil || val < 0 {
+			continue
+		}
+		flags.FieldNums = append(flags.FieldNums, val)
+	}
+
+	if len(flags.FieldNums) == 0 {
+		flag.Usage()
+	}
+
+	return flags
+}
+
+func Cut(flags Flags, inp string) []string {
+	// Разделение
+	strs := strings.Split(inp, flags.Delimiter)
+	// Если нет разделитиля и поднят флаг -s - ничего не выводим
+	if flags.Separated && len(strs) == 1 {
+		return nil
+	}
+
+	res := make([]string, 0, len(flags.FieldNums))
+	// Копирование нужных полей
+	for _, v := range flags.FieldNums {
+		if v > len(strs) || v <= 0 {
+			continue
+		}
+		res = append(res, strs[v-1])
+	}
+
+	return res
+}
+
+func main() {
+	flags := GetFlags()
+	for {
+		var inp string
+		// Чтение всего stdin
+		n, err := fmt.Scanln(&inp)
+		if err != nil && !errors.Is(err, io.EOF) {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		if n <= 0 {
+			break
+		}
+
+		fmt.Println(strings.Join(Cut(flags, inp), flags.Delimiter))
+	}
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"slices"
+	"testing"
+)
+
+type TestCase struct {
+	flags  Flags
+	input  string
+	output []string
+}
+
+func runTests(t *testing.T, prefix string, cases []TestCase) {
+	for i, tc := range cases {
+		out := Cut(tc.flags, tc.input)
+		if slices.Compare(out, tc.output) != 0 {
+			t.Errorf("%s: Test %d: Expected %#v Got %#v", prefix, i, tc.output, out)
+		}
+	}
+}
+
+func TestCut(t *testing.T) {
+	cases := []TestCase{
+		// Простой тест на обрезание полей
+		{Flags{FieldNums: []int{1, 3}, Delimiter: "\t"}, "123\t456\t789", []string{"123", "789"}},
+		// Тот же тест, но с запятыми
+		{Flags{FieldNums: []int{1, 3}, Delimiter: ","}, "123,456,789", []string{"123", "789"}},
+		// Тест с полями за пределами массива
+		{Flags{FieldNums: []int{1, 3, 6, 7}, Delimiter: "\t"}, "123\t456\t789", []string{"123", "789"}},
+		// Тест с поднятыми флагом -s
+		{Flags{FieldNums: []int{1, 3, 6, 7}, Delimiter: "\t", Separated: true}, "123,456,789", nil},
+	}
+
+	runTests(t, "AIO", cases)
+}
